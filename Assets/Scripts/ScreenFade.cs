12259f056423121df6b1d45633ba4ce4174c0670
Andrew Morris <jump3r@dggaming.net>
2014-10-06 04:14:28 -0400

    Added Intro Scene and Text/Scene fade transitions (rough).

---
+++ Assets/Scripts/ScreenFade.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenFade : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;
+	private bool sceneStarting = true;
+	// Use this for initialization
+	void Awake () {
+		guiTexture.pixelInset = new Rect (0f, 0f, Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sceneStarting) {
+			StartScene ();
+		}
+	}
+
+	void FadeToClear() {
+		guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeToBlack() {
+		guiTexture.color = Color.Lerp (guiTexture.color, Color.black, fadeSpeed * Time.deltaTime);
+	}
+
+	void StartScene() {
+		FadeToClear ();
+		if (guiTexture.color.a < 0.05f) {
+			guiTexture.color = Color.clear;
+			guiTexture.enabled = false;
+			sceneStarting = false;
+		}
+	}
+
+	void EndScene() {
+		guiTexture.enabled = true;
+		FadeToBlack ();
+	}
+
+}
---
+++ Assets/Scripts/TextFader.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFader : MonoBehaviour {
+
+	public float fadeSpeed = 1.5f;
+	public bool changeScene;
+
+	private float timer = 0f;
+	private TextMesh text;
+
+	private bool startFadeIn = false;
+	private bool startFadeOut = false;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startFadeIn) {
+			StartFade ();
+			Debug.Log ("1s, starting Fade In");
+		}
+
+		else if (startFadeOut) {
+			EndFade ();
+			Debug.Log("7s, starting Fade Out");
+		}
+
+		if (timer > 1f && timer < 7f && !startFadeIn) {
+			Debug.Log("Setting StartFadeIn to true");
+			startFadeIn = true;
+		}
+
+		if (timer > 7f) {
+			startFadeOut = true;
+		}
+
+		timer += Time.deltaTime;
+	}
+
+	void FadeTextIn() {
+		text.color = Color.Lerp (text.color, Color.white, fadeSpeed * Time.deltaTime);
+	}
+
+	void FadeTextOut() {
+		text.color = Color.Lerp (text.color, Color.clear, fadeSpeed * Time.deltaTime);
+	}
+
+	void StartFade() {
+		FadeTextIn ();
+		if (text.color.a > 0.90f) {
+			text.color = Color.white;
+			startFadeIn = false;
+			Debug.Log ("The Color is " + text.color.a + "please set me to false");
+		}
+	}
+
+	void EndFade() {
+		FadeTextOut ();
+		if (text.color.a < 0.15f) {
+			text.color = Color.clear;
+			startFadeOut = false;
+			if (changeScene) {
+				Application.LoadLevel (1);
+			}
+		}
+	}
+}
