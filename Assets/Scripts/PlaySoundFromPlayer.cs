841ce88dd7f8bedb3d468e4bfa9d310488bbf850
Andrew Morris <jump3r@dggaming.net>
2014-11-03 22:19:41 -0500

    Updated build for Colin's playtesting. v1.0.1103

---
+++ Assets/Scripts/PlaySoundFromPlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundFromPlayer : MonoBehaviour {
+
+	public AudioClip[] soundClips;
+	private bool started = false;
+	private bool finished = false;
+	private float delayTimeToDestroy = 1f;
+
+	// Use this for initialization
+	void Start () {
+		RandomPlay ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (started && !audio.isPlaying && delayTimeToDestroy < 0f) {
+			// Destroy (gameObject);
+			Debug.Log ("Destroy Audio Object");
+			PhotonNetwork.Destroy (gameObject);
+		}
+
+		delayTimeToDestroy -= Time.deltaTime;
+	}
+
+	void RandomPlay() {
+		int c = Random.Range(0, soundClips.Length);
+		if (!audio.isPlaying) {
+			audio.clip = soundClips[c];
+			audio.Play();
+			started = true;
+			Debug.Log("Audio Clip " + c + " has played");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerSoundInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSoundInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Pressing 'f1' plays a sound.
+		if (Input.GetKeyDown (KeyCode.F1)) {
+			Debug.Log ("Instantiate New Sound Object");
+			PhotonNetwork.Instantiate ("PlayerSound", gameObject.transform.position, Quaternion.identity, 0);
+		}
+	}
+}
